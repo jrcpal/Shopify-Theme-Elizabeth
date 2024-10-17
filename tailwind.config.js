9c16afeae0af68c5ef1a1cea1299ce7fe04fc8f6
Jessica Churgin <jchurgy@gmail.com>
2024-10-17 13:52:17 -0400

    adding tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './layout/*.liquid'
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
   ],
   theme: {
     extend: {},
